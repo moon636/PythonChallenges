4a9ea931bcbf7cca69b7f0b2726b4320422bc402
Shahzad <M2201882@middlesbro.ac.uk>
2022-10-03 15:30:39 +0100

    update

--- L7challenges.py
+++ L7challenges.py
@@ -2,3 +2,48 @@ t1 = [1,2,3,4,5]
 t2 = ["a","b","c","d","e"]
 
 print(dict(zip(t1,t2)))
+
+#######################################################
+
+sampleDict = {
+    "class":{
+        "student":{
+            "name":"Mike",
+            "marks":{
+                "physics":70,
+                "history":80
+                }
+            }
+        }
+    }
+
+print(sampleDict["class"]["student"]["marks"]["history"])
+
+#######################################################
+
+sampleDict = {
+    "name":"Kelly",
+    "age":25,
+    "salary":8000,
+    "city":"New york"
+    }
+
+keysToRemove = ["name","salary"]
+
+for item in keysToRemove:
+    sampleDict.pop(item)
+
+print(sampleDict)
+
+#######################################################
+
+sampleDict = {
+    "name":"Kelly",
+    "age":25,
+    "salary":8000,
+    "city":"New york"
+    }
+
+sampleDict["wage"] = sampleDict.pop("salary")
+
+print(sampleDict)
